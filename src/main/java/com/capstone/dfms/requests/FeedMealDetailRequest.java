38db061caf93c53119b7d98977dc70a26a5cea97
tranthihongphuc <hongphuc0987@gmail.com>
2025-02-20 23:17:46 +0700

    fix cow type

--- src/main/java/com/capstone/dfms/requests/FeedMealDetailRequest.java
+++ src/main/java/com/capstone/dfms/requests/FeedMealDetailRequest.java
@@ -8,13 +8,15 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.math.BigDecimal;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 public class FeedMealDetailRequest {
 
-    private Long quantity;
+    private BigDecimal quantity;
 
     private Long itemId;
 
