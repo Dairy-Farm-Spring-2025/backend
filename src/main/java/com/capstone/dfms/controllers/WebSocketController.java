7fa994a18bf2fb8a266efe687a98ebce60b968cc
tranthihongphuc <hongphuc0987@gmail.com>
2025-03-22 17:54:24 +0700

    Fix notification

--- src/main/java/com/capstone/dfms/controllers/WebSocketController.java
+++ src/main/java/com/capstone/dfms/controllers/WebSocketController.java
@@ -2,8 +2,6 @@ package com.capstone.dfms.controllers;
 
 import com.capstone.dfms.models.NotificationEntity;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
-import org.springframework.messaging.simp.SimpMessageType;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Component;
 
@@ -16,10 +14,12 @@ public class WebSocketController {
     private SimpMessagingTemplate messagingTemplate;
 
     public void sendListNotificationUpdate(Long userId, List<NotificationEntity> notifications) {
-
+//        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+//        UserPrincipal userPrincipal = (UserPrincipal) authentication.getPrincipal();
+//        UserEntity user = userPrincipal.getUser();
         messagingTemplate.convertAndSendToUser(
                 String.valueOf(userId),
-                "/queue/notifications",
+                "/queue/notifications/"+ userId,
                 notifications
         );
     }
