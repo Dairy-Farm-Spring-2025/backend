91276b12fca15aee4373f626aa5377dcf22b6911
tranthihongphuc <hongphuc0987@gmail.com>
2025-03-24 23:17:08 +0700

    Fire base config

--- src/main/java/com/capstone/dfms/components/configurations/FirebaseConfig.java
+++ src/main/java/com/capstone/dfms/components/configurations/FirebaseConfig.java
@@ -8,8 +8,10 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.io.ByteArrayInputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 @Configuration
 public class FirebaseConfig {
@@ -19,10 +21,13 @@ public class FirebaseConfig {
 
     @Bean
     public FirebaseApp firebaseApp() throws IOException {
-        FileInputStream serviceAccount = new FileInputStream(serviceAccountPath);
+
+        //FileInputStream serviceAccount = new FileInputStream(serviceAccountPath);
+        ByteArrayInputStream serviceAccountStream =
+                new ByteArrayInputStream(serviceAccountPath.getBytes(StandardCharsets.UTF_8));
 
         FirebaseOptions options = FirebaseOptions.builder()
-                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .setCredentials(GoogleCredentials.fromStream(serviceAccountStream))
                 .build();
 
         if (FirebaseApp.getApps().isEmpty()) {
