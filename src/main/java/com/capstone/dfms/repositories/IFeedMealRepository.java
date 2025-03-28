ab44bb37e0e88a5cfe7a12c404634067edd02d5c
tranthihongphuc <hongphuc0987@gmail.com>
2025-03-28 21:21:22 +0700

    Calculate food for area

--- src/main/java/com/capstone/dfms/repositories/IFeedMealRepository.java
+++ src/main/java/com/capstone/dfms/repositories/IFeedMealRepository.java
@@ -1,7 +1,17 @@
 package com.capstone.dfms.repositories;
 
+import com.capstone.dfms.models.CowTypeEntity;
 import com.capstone.dfms.models.FeedMealEntity;
+import com.capstone.dfms.models.enums.CowStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface IFeedMealRepository extends JpaRepository<FeedMealEntity, Long> {
+    @Query("SELECT f FROM FeedMealEntity f WHERE f.cowTypeEntity = :cowType AND f.cowStatus = :cowStatus")
+    Optional<FeedMealEntity> findByCowTypeAndStatus(@Param("cowType") CowTypeEntity cowType, @Param("cowStatus") CowStatus cowStatus);
 }
