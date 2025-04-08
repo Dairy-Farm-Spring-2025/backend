823c64533aca5c01635df77d9d6d7e18df5b440e
chinhhn173672 <ngocchinhpy23@gmail.com>
2025-04-08 21:36:08 +0700

    [0406] Fix Vaccine Cycle Detail

---
+++ src/main/java/com/capstone/dfms/requests/VaccineCycleDetailUpdateRequest.java
@@ -0,0 +1,38 @@
+package com.capstone.dfms.requests;
+
+import com.capstone.dfms.models.enums.InjectionSite;
+import com.capstone.dfms.models.enums.ItemUnit;
+import com.capstone.dfms.models.enums.UnitPeriodic;
+import com.capstone.dfms.models.enums.VaccineType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class VaccineCycleDetailUpdateRequest {
+    private String name;
+
+    private String description;
+
+    private ItemUnit dosageUnit;
+
+    private String vaccineIngredients;
+
+    private VaccineType vaccineType;
+
+    private double dosage;
+
+    private InjectionSite injectionSite;
+
+    private UnitPeriodic unitPeriodic;
+
+    private Integer numberPeriodic;
+
+    private Long itemId;
+
+    private Integer firstInjectionMonth;
+}
