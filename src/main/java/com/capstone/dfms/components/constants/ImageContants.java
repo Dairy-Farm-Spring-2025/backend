e6824884248c6912ac25671aa2c2528fa7ae178e
tranthihongphuc <hongphuc0987@gmail.com>
2025-04-10 16:37:33 +0700

    List cow for area + feed meal

--- src/main/java/com/capstone/dfms/components/constants/ImageContants.java
+++ src/main/java/com/capstone/dfms/components/constants/ImageContants.java
@@ -1,7 +1,7 @@
 package com.capstone.dfms.components.constants;
 
 public class ImageContants {
-    public static final String DEFAULT_AVATAR = "default.jpg";
+    public static final String DEFAULT_AVATAR = "images/users/default.jpg";
 
     public static final String USERS_IMAGE_PATH = System.getProperty("user.dir") + "/uploads/users";
     public static final String REPORT_IMAGE_PATH = System.getProperty("user.dir") + "/uploads/reportTasks";
